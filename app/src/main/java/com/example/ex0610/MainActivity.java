fea4c531acd5ada449da083bac6f0cb3abdbbe7a
av2215 <av2215@bs.amalnet.k12.il>
2025-04-10 18:45:52 +0300

    סיום עיצוב

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex0610;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +15,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ToggleButton tb;
+    Switch sw;
+    Button bt;
+    LinearLayout main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tb = findViewById(R.id.tb);
+        sw = findViewById(R.id.sw);
+        bt = findViewById(R.id.bt);
+        main = findViewById(R.id.main);
+    }
+
+
+    public void go(View view) {
+
     }
 }
